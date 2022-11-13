1b30097bb6af12d8967ecc748c79063edb279232
Théo Reumont <66231672+Theoreumont@users.noreply.github.com>
2022-11-13 15:37:23 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>
+    App
+  </h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      
+    }
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
